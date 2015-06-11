8e1a3f6ddc3940563a0c2ea969f854f08f94118e
Ryan Doyle <ryan@doylenet.net>
2015-06-11 23:02:00 +1000

    Hookup entry completion to spell checker

--- lazespell.c
+++ lazespell.c
@@ -4,11 +4,19 @@
 
 
 
-void entry_changed_event(gpointer *entry) {
+void entry_changed_event(GtkWidget *entry, GtkEntryCompletion *completion) {
+
+    GtkTreeModel *model = gtk_entry_completion_get_model(completion);
+    gtk_list_store_clear(GTK_LIST_STORE(model));
+
+    GtkTreeIter iter;
+
+
     const char *word = gtk_entry_get_text(GTK_ENTRY(entry));
     printf("%s\n", word);
 
     SpellResult *result = new_spell_result(word);
+
 
     printf("CORRRECTIONS FOR: %s\n", word);
 
@@ -16,40 +24,39 @@ void entry_changed_event(gpointer *entry) {
     while ( (correction = aspell_string_enumeration_next(result->possible_corrections)) != NULL )
     {
         printf("%s\n", correction);
+        gtk_list_store_append(GTK_LIST_STORE(model), &iter);
+        gtk_list_store_set(GTK_LIST_STORE(model), &iter, 0, correction, -1);
     }
 
     destroy_spell_result(result);
 
 }
 
+gboolean noop_match(GtkEntryCompletion *completion,
+                    const gchar *key,
+                    GtkTreeIter *iter,
+                    gpointer user_data) {
+    return TRUE;
+}
+
 int main(int argc, char **argv) {
-
-    char *to_check = argv[1];
-
-    SpellResult *result = new_spell_result(to_check);
-
-    if(result->is_correct) {
-        puts("word is correct");
-    } else {
-        puts("word is incorrect");
-    }
-
-    const char *word;
-    while ( (word = aspell_string_enumeration_next(result->possible_corrections)) != NULL )
-    {
-        printf("%s\n", word);
-    }
 
     /* GTK stuffs */
     GtkWidget *entry;
     GtkWidget *window;
+    GtkListStore *model;
+    GtkEntryCompletion *completion;
 
     gtk_init(&argc, &argv);
+
+    model = gtk_list_store_new(1, G_TYPE_STRING);
+    completion = gtk_entry_completion_new();
+
 
     entry = gtk_entry_new();
     gtk_entry_set_text(GTK_ENTRY(entry), "derps");
 
-    g_signal_connect(entry, "changed", G_CALLBACK(entry_changed_event), entry);
+    g_signal_connect(entry, "changed", G_CALLBACK(entry_changed_event), completion);
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     g_signal_connect(window, "delete-event", G_CALLBACK(gtk_main_quit), NULL);
@@ -61,26 +68,15 @@ int main(int argc, char **argv) {
 
     gtk_widget_show_all (window);
 
-    GtkEntryCompletion *completion;
-    completion = gtk_entry_completion_new();
+
     gtk_entry_completion_set_text_column(completion, 0);
     gtk_entry_set_completion(GTK_ENTRY(entry), completion);
 
-    GtkListStore *model;
-    GtkTreeIter iter;
-    model = gtk_list_store_new(1, G_TYPE_STRING);
-
-    gtk_list_store_append(model, &iter);
-    gtk_list_store_set(model, &iter, 0, "Doops", -1);
-
-    gtk_list_store_append(model, &iter);
-    gtk_list_store_set(model, &iter, 0, "Fooper", -1);
-
+    gtk_entry_completion_set_match_func(completion, noop_match, NULL, NULL);
     gtk_entry_completion_set_model(completion, GTK_TREE_MODEL(model));
+    gtk_entry_completion_set_minimum_key_length(completion, 3);
 
     gtk_main();
-
-    destroy_spell_result(result);
 
     return 0;
 }
